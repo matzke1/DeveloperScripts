8d79ab575e304c0a658a921171baccf56436cf97
Robb Matzke <matzke1@llnl.gov>
2016-03-01 10:10:05 -0500

    RMC: Added some extra libraries needed on RHEL 6 at the lab
    These library directories are always the same across all RHEL 6 configurations.
    In fact, they should therefore probably be in the user's environment instead
    of being set by RMC.

--- rose-devel-tools/rmc-support/modules/os.sh
+++ rose-devel-tools/rmc-support/modules/os.sh
@@ -34,4 +34,22 @@ rmc_os_check() {
 	echo "$arg0: unknown operating system name (use rmc_os to set it)" >&2
 	exit 1
     fi
+
+    # Add extra libraries
+    local extra_libs=()
+    case "$RMC_OS_NAME_FILE" in
+	Red_Hat_Enterprise_Linux_Workstation_release_6.7__Santiago_)
+	    extra_libs=(
+		/nfs/casc/overture/ROSE/opt/rhel6/x86_64/gcc/4.8.1/mpc/1.0/mpfr/3.1.2/gmp/5.1.2/lib64
+		/nfs/casc/overture/ROSE/opt/rhel6/x86_64/gmp/5.1.2/gcc/4.4.7/lib
+		/nfs/casc/overture/ROSE/opt/rhel6/x86_64/mpc/1.0/gcc/4.4.7/mpfr/3.1.2/gmp/5.1.2/lib
+		/nfs/casc/overture/ROSE/opt/rhel6/x86_64/mpfr/3.1.2/gcc/4.4.7/gmp/5.1.2/lib
+	    )
+	    ;;
+    esac
+
+    local f
+    for f in "${extra_libs[@]}"; do
+	LD_LIBRARY_PATH=$(rmc_adjust_list prepend_or_move "$f" : "$LD_LIBRARY_PATH")
+    done
 }
